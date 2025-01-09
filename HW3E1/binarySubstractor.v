397a8cb5e40590e5b87792975a3c83fdfd32751e
orkeren <orkeren8@gmail.com>
2025-01-09 22:08:12 +0200

    fix binary subtractor logic

--- HW3E1/binarySubstractor.v
+++ HW3E1/binarySubstractor.v
@@ -1,19 +1,28 @@
 `timescale 1ns/1ns
 
-module binarySubstractor #(parameter WIDTH=1) (
-  output bOut,
-  output [WIDTH-1:0] sub,
-  input  [WIDTH-1:0] x,
-  input  [WIDTH-1:0] y
+module binarySubstractor #(N) (
+  output [N-1:0] z,
+  output b,
+  output v,
+  input  [N-1:0] x, y,
+  input  bIn
 );
-  wire [WIDTH-1:0] b;
+  wire [N-1:0] b;
+  wire xor1,xor2;
 
-  assign bOut = b[WIDTH-1];
+  assign b = b[N-1];
 
-  fullSubtractor fs(b[0],sub[0],x[0],y[0],1'b0);
+  fullSubtractor fs(b[0],z[0],x[0],y[0],bIn);
 
-  for (genvar i = 1; i < WIDTH; i = i + 1)
+  for (genvar i = 1; i < N; i = i + 1)
   begin
-    fullSubtractor fs(b[i],sub[i],x[i],y[i],b[i-1]);
+    fullSubtractor fs(b[i],z[i],x[i],y[i],b[i-1]);
   end
+
+  assign b = b[N-1];
+
+  xorGate x1(xor1, z[N-1],x[N-1]);
+  xorGate x2(xor2, y[N-1],x[N-1]);
+  andGate a1(v,xor1,xor2);
+
 endmodule
